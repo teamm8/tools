dc1c823c5c4e0e00548fd1b5fa7a384e4d46f22f
teamm8 <leonjaeggi@gmail.com>
2018-04-27 20:51:10 +0000

    modified:   removeCsvHeader.py 	headersRemoved/

--- removeCsvHeader.py
+++ removeCsvHeader.py
@@ -3,7 +3,7 @@
 
 import csv, os
 
-os.makedirs('headersRemoved'. exist_ok=True)
+os.makedirs('headersRemoved', exist_ok=True)
 
 # Loop through every file in cwd
 for csvFilename in os.listdir('.'):
@@ -12,6 +12,20 @@ for csvFilename in os.listdir('.'):
 
     print ("Removing header from", csvFilename, '...')
 
-    # TODO: read the csv file in
+    # read the csv file in
+    csvRows = []
+    csvFile = open(csvFilename)
+    reader = csv.reader(csvFile)
+    for row in reader:
+        if reader.line_num==1:
+            continue # skip the first row
+        csvRows.append(row)
+    csvFile.close()
 
-    # TODO: write out the new csv file
+    # write out the new csv file
+    print ("Writing to", os.path.join('headersRemoved', csvFilename))
+    csvFile = open(os.path.join('headersRemoved', csvFilename), 'w', newline='')
+    csvWriter = csv.writer(csvFile)
+    for row in csvRows:
+        csvWriter.writerow(row)
+    csvFile.close()
