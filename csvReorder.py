c416f170e2b544708e9fc6e05f97bd7aeecded09
teamm8 <leonjaeggi@gmail.com>
2018-04-28 23:35:07 +0000

    modified:   csvReorder.py

--- csvReorder.py
+++ csvReorder.py
@@ -7,17 +7,45 @@ FILTERFILE =
 RAWFILE =
 OUTPUTFILE =
 
+# open the raw file
+csvRawFile = open(RAWFILE)
+rawReader = csv.reader(csvRawFile)
 
 # open the filter file
 csvFilterFile = open(FILTERFILE)
 filterReader = csv.reader(csvFilterFile)
 
-# open the raw file
-csvRawFile = open(RAWFILE)
-rawReader = csv.reader(csvRawFile)
-
 # open the output file
 csvOutputFile = open(OUTPUTFILE, 'w', newline='')
 outputWriter = csv.writer(csvOutputFile)
 
+def getraw(filename, criterion):
+    with open(filename) as csvfile:
+        datareader = csv.reader(csvfile)
+        count = 0
+        for row in datareader:
+            if row[3] in ("column header", criterion):
+                yield row
+                count += 1
+            elif count < 2:
+                continue
+            else:
+                return
+
+def getdata(filename, criteria):
+    for criterion in criteria:
+        for row in getraw(filename, criterion):
+            yield row
+
+for row in getdata(somefilename, sequence_of_criteria):
+    # process row
+
+
+
 # dump a smaller section of the larger file
+for rawRow in rawReader:
+    if rawReader.line_num == 1:
+        outputWriter.writeline(
+
+
+
